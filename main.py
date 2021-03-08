316945932d90f3835cd663b49e7e2453e9da4128
trl242 <royaurallockhart@gmail.com>
2021-03-08 05:16:55 -0800

    hame page other than link to posts done

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    blog_response = requests.get(blog_url)
+    blog_data = blog_response.json()
+
+    return render_template("index.html", posts=blog_data)
+
+@app.route('/post/<id>')
+def blog_post():
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    blog_response = requests.get(blog_url)
+    blog_data = blog_response.json()
+
+    return render_template("index.html", posts=blog_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
