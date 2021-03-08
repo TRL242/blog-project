47f4c67b2758b8045bc81a9c6bfe0e8b41df7c68
trl242 <royaurallockhart@gmail.com>
2021-03-08 05:33:23 -0800

    posts and home page work

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from flask import Flask, render_template
+from post import Post
 import requests
 
+posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
-    blog_response = requests.get(blog_url)
-    blog_data = blog_response.json()
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
 
-    return render_template("index.html", posts=blog_data)
 
-@app.route('/post/<bog_id>')
-def blog_post():
-    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
-    blog_response = requests.get(blog_url)
-    blog_data = blog_response.json()
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
-    return render_template("index.html", posts=blog_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
